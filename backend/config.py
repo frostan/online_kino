f7c5276b80f649e2c94c8dee9c54189d9d10f54f
Антон <frostik96@gmail.com>
2025-08-08 13:38:08 +0300

    feat:middleware

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     DB_URL: str
     SECRET_KEY: str
-    model_config = SettingsConfigDict(env_file='.env')
+
+    BASE_DIR: Path = Path(__file__).parent.resolve()
+    MEDIA_DIR: Path = BASE_DIR / 'media'
+
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8', extra='ignore'
+    )
 
 
 settings = Settings()
